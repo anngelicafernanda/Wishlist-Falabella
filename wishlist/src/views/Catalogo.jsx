0487d573b04c32c9c5eec538016c906069d35462
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-08-22 14:12:14 -0400

    get-images-catalog

--- wishlist/src/views/Catalogo.jsx
+++ wishlist/src/views/Catalogo.jsx
@@ -1,13 +1,36 @@
 //traer fotos: trini
 //estilo foto: ange y key
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import { Link } from "react-router-dom";
 
+
 export function Catalogo() {
+
+    const [products, setProducts] = useState([])
+
+    const getProduct = async () => {
+     try {
+       const response = await axios.get('https://raw.githubusercontent.com/ivanmirson/hackathon-laboratoria/main/wishlist/data-wishlist.json');
+       return setProducts(response.data.products)
+     } catch (error) {
+       console.error(error);
+     }
+   }
+   
+     useEffect(()=>{
+       getProduct();
+     },[]) 
+
+
   return (
     <div>Catalogo
          <Link className='bg-orange-500' to="/Mis-Listas">Mis Listas</Link>
+         {
+       products.map((p)=>
+        <img src={p.images[0]}></img>
+      )}  
     </div> 
   )
 }
