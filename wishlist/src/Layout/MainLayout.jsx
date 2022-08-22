e5af90fbb2036f52afaa2b6e4096cd68ffab69bf
Angelica Contreras <afernandac94@gmail.com>
2022-08-22 17:15:20 -0400

    Logo

--- wishlist/src/Layout/MainLayout.jsx
+++ wishlist/src/Layout/MainLayout.jsx
@@ -4,13 +4,13 @@ import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { Copyright } from '../components/Copyright';
 
-export function MainLayout({children}) {
-  return (
-    <>
-    <Header />
-    {children}
-    <Footer />
-    <Copyright />
-    </>
-  )
+export function MainLayout({ children }) {
+	return (
+		<>
+			<Header />
+			{children}
+			<Footer />
+			<Copyright />
+		</>
+	);
 }
--- wishlist/src/components/Header.jsx
+++ wishlist/src/components/Header.jsx
@@ -1,8 +1,18 @@
 //ange
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from './Logo.svg';
 
 export function Header() {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<header className="">
+			<div className="bg-orange h-[70px] grid grid-flow-col auto-cols-menu text-white">
+				<div className="bg-orangeFalabella h-[70px] rounded-r-full grid place-content-center pl-[48px] pr-[24px]">
+					<Link to="/">
+						<Logo />
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
