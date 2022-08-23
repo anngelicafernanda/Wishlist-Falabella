f1c1b4823cd57a7dbaf0a09eda99ef89d9b63aa4
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-08-23 11:17:17 -0400

    resolve-conflic-lista-detalles

--- wishlist/src/views/ListaDetalle.jsx
+++ wishlist/src/views/ListaDetalle.jsx
@@ -32,51 +32,6 @@ export function ListaDetalle() {
     }
     
     return (
-    <div>
-        ListaDetalle
-        <Link className="bg-orange-500" to="/">
-          Agregar producto
-        </Link>
-        <div className="bg-gray-light  sticky w-screen h-screen m-0 border-slate-300    mb-3 mx-8">
-          <div className=" font-style::lato text-m content-start font-bold pb-[13px] text-base border-b text-color-border-b flex items-center border-text-color-border-b">
-            {/* aqui va un icono back */}
-            <p>Volver a mis listas</p>
-          </div>
-          <div className="grid grid-flow-col border-b justify-between mt-[70px] pb-[13px] border-text-color-border-b">
-            <div className="grid grid-flow-col gap-[15px]">
-              <h1 className="font-style:lato font-bold text-base">
-                Compras del mes
-              </h1>
-              <div className="justify-center  space-y-{amount} flex items-center border-text-color-eliminar">
-                <p className="font-style:lato text-xs text-base flex-row-reverse underline text-text-color-eliminar">
-                  Editar
-                </p>
-              </div>
-            </div>
-            <p
-              className="font-sans text-xs text-base text-color-eliminar text-right mr-8 underline
-          text-text-color-eliminar"
-            >
-              Eliminar lista
-            </p>
-          </div>
-          <div className=" flex justify-between border-b pb-[13px] mt-[21px] border-text-color-border-b">
-            <h1>No hay Productos en esta Lista</h1>
-
-            <button className=" flex justify-between border-solid pb-[14px] ">
-              <ActionButton
-                className="btn-orange text-white text-[14px] "
-                title="Agrega productos a la lista"
-                callback={alert}
-              />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div>
-        <MenuAside />
-      </div>
-    </>
         <div>
             <div className='flex justify-between mx-10 my-5'>
                 <Link className='text-xs' to="/Mis-Listas">Volver a Mis Listas</Link>      
@@ -100,9 +55,52 @@ export function ListaDetalle() {
                     </button>
                 </div>
             )}
-                  <div>
-        <MenuAside />
-      </div>
         </div>
     )
 }
+
+{/* <div>
+ListaDetalle
+<Link className="bg-orange-500" to="/">
+  Agregar producto
+</Link>
+<div className="bg-gray-light  sticky w-screen h-screen m-0 border-slate-300    mb-3 mx-8">
+  <div className=" font-style::lato text-m content-start font-bold pb-[13px] text-base border-b text-color-border-b flex items-center border-text-color-border-b"> */}
+    {/* aqui va un icono back */}
+{/*     <p>Volver a mis listas</p>
+  </div>
+  <div className="grid grid-flow-col border-b justify-between mt-[70px] pb-[13px] border-text-color-border-b">
+    <div className="grid grid-flow-col gap-[15px]">
+      <h1 className="font-style:lato font-bold text-base">
+        Compras del mes
+      </h1>
+      <div className="justify-center  space-y-{amount} flex items-center border-text-color-eliminar">
+        <p className="font-style:lato text-xs text-base flex-row-reverse underline text-text-color-eliminar">
+          Editar
+        </p>
+      </div>
+    </div>
+    <p
+      className="font-sans text-xs text-base text-color-eliminar text-right mr-8 underline
+  text-text-color-eliminar"
+    >
+      Eliminar lista
+    </p>
+  </div>
+  <div className=" flex justify-between border-b pb-[13px] mt-[21px] border-text-color-border-b">
+    <h1>No hay Productos en esta Lista</h1>
+
+    <button className=" flex justify-between border-solid pb-[14px] ">
+      <ActionButton
+        className="btn-orange text-white text-[14px] "
+        title="Agrega productos a la lista"
+        callback={alert}
+      />
+    </button>
+  </div>
+</div>
+</div>
+<div>
+<MenuAside />
+</div>
+</>  */}
