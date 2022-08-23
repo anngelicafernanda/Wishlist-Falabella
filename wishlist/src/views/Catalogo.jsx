953fda8f980547d0603d5ca785c6a6fdde358a7d
fabibbc <fbelenbc@gmail.com>
2022-08-22 23:26:24 -0400

    dropdownList

--- wishlist/src/views/Catalogo.jsx
+++ wishlist/src/views/Catalogo.jsx
@@ -5,6 +5,7 @@ import React, {useState, useEffect, useContext} from 'react'
 import axios from 'axios'
 import { Link } from "react-router-dom";
 import { ListContext } from '../context/ListContext';
+import Select from 'react-select';
 
 
 export function Catalogo() {
@@ -26,25 +27,42 @@ export function Catalogo() {
        getLists();
      },[]) 
 
-     const addProduct = (list, product) =>{
-        updateList(list.docId, product);
-        alert(`Tu producto ha sido agregado a ${list.name}`)
-     }
+      const addProduct = (list, product) =>{
+          updateList(list.docId, product);
+          alert(`Tu producto ha sido agregado a ${list.name}`)
+      }
 
-
+ /* ORDENAR TAILWIND */
   return (
-    <div>
+    <div className='w-full my-10'>
         <div>VISTA CATÁLOGO</div>
         <Link className='bg-orange-500' to="/Mis-Listas">Mis Listas</Link>
-        {products.map((p)=>
-        <div key={p.productId}>
-            <img src={p.images[0]}></img>
-            {lists.map((list)=>
-                <div key={list.docId} onClick={()=>{addProduct(list, p)}}>{list.name}</div>
-            )}
-            
+        <div className='flex flex-wrap justify-center items-center'>
+          {products.map((p)=>
+          <div className='mx-3 my-3  w-[228px] h-[467px] bg-white overflow-y-hidden' key={p.productId}>
+              <img className='w-[228px] h-[228px]' src={p.images[0]}></img>
+              <p>+ SUSTENTABLE</p>
+              <p>{p.brand}</p>
+              <p>{p.name}</p>
+              <p>Por Falabella</p>
+              <p>${p.offerings[0].price.toLocaleString('de-DE')}</p>
+              <div className='flex flex-col items-center'>
+                <button className='btn-orange text-[16px] px-10 mt-0 mb-0'>Agregar Productos</button>
+                <select name="" id="">
+                  <option>Agregar a tu lista</option>
+                  {lists.map((list)=>
+                    <option key={list.docId} onClick={()=>{addProduct(list, p)}}>{list.name}</option>
+                  )}
+                </select>
+              </div>
+          </div>
+          )}  
         </div>
-        )}  
     </div> 
   )
 }
+
+
+// {lists.map((list)=>
+//   <div key={list.docId} onClick={()=>{addProduct(list, p)}}>{list.name}</div>
+// )}
