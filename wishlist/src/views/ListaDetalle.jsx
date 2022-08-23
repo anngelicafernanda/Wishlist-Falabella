4f0ce041b3e2ce44b773f20f37d2013ce9ba6b10
fabibbc <fbelenbc@gmail.com>
2022-08-22 20:48:41 -0400

    popup Delete

--- wishlist/src/views/ListaDetalle.jsx
+++ wishlist/src/views/ListaDetalle.jsx
@@ -1,16 +1,18 @@
 //key
-
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Popup } from '../components/Popup'
 
 export function ListaDetalle() {
 
+  const [popUp, setPopUp] = useState(false);
 
   return (
-    <div>ListaDetalle
-        
+    <div>ListaDetalle       
         <Link className='bg-orange-500' to="/">Agregar producto</Link>
-
+        <button onClick={() => setPopUp(true)}>Eliminar</button>
+        <Popup trigger={popUp} setTrigger={setPopUp} title={<h4>Eliminar Producto</h4>} desc={<p>Estas apunto de elimnar un producto de la lista</p>} btnName={"Aceptar"} /*clickFunction ={deleteList}*/ />
     </div>
   )
 }
