58d49b8c5e7c59b9139c9a8974cfb1729d4405b1
Angelica Contreras <afernandac94@gmail.com>
2022-08-25 17:56:46 -0400

    Imagenes lista vacia

--- wishlist/src/components/InformationCard.jsx
+++ wishlist/src/components/InformationCard.jsx
@@ -1,10 +1,10 @@
 export const InformationCard = ({ title, description, icon }) => {
 	return (
 		<div className="bg-container flex  p-4">
-			<img className="w-[39px]" src={icon} alt="" />
+			{icon}
 			<div className="m-2">
-				<h1 className="font-bold text-[18px] text-[#333333]">{title}</h1>
-				<p className=" text-info text-[18px] text-[#4A4A4A]">{description}</p>
+				<h1 className="font-bold text-[18px]">{title}</h1>
+				<p className=" text-info text-[18px] ">{description}</p>
 			</div>
 		</div>
 	);
