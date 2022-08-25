8d09f763af2bd23c824bf4458abed2a421ff901e
Angelica Contreras <afernandac94@gmail.com>
2022-08-25 18:09:27 -0400

    Link eliminado de menu

--- wishlist/src/components/Dropdown.jsx
+++ wishlist/src/components/Dropdown.jsx
@@ -1,16 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import downarrow from '../imgFalabella/down-arrow-search.svg';
 import check from '../images/check.png';
 
-export function Dropdown({lists, product, handleSelect}) {
-    const [dropdown, setDropdown] = useState(false)
-    const [id, setId] = useState('')
+export function Dropdown({ lists, product, handleSelect }) {
+	const [dropdown, setDropdown] = useState(false);
+	const [id, setId] = useState('');
 
-    const handleClick = (e) =>{
-        handleSelect(e.currentTarget.id,product)
-        setDropdown(false)
-    }
-/* 
+	const handleClick = (e) => {
+		handleSelect(e.currentTarget.id, product);
+		setDropdown(false);
+	};
+	/* 
     const handleBtnClick = (e) =>{
         setDropdown(!dropdown); 
         setId(e.target.id);
@@ -21,28 +21,58 @@ export function Dropdown({lists, product, handleSelect}) {
         }
     } */
 
-    return (
-        <div id={product.productId}className='w-[200px] m-0-auto relative'> 
-            <div id={product.productId} className=' p-[10px] shadow bg-white flex justify-between w-full rounded-full mt-2 h-10 items-center cursor-pointer '
-            onClick={(e)=>{setDropdown(!dropdown)}}> Agregar a la lista
-                <img src={downarrow} className="color-black w-3"/>
-            </div>
-            {dropdown && 
-                <div className='absolute p-[5px] shadow mt-0.5 text-sm w-full z-10 bg-white'>
-                    {lists.map((list)=>
-                        list.productsId.includes(product.productId) ? (
-                            <div className='hover:bg-gray-light cursor-pointer flex justify-between p-1 border-b border-gray-light ' id='disabled' onClick={(e)=>{handleClick(e)}}>{list.name} 
-                                <img src={check} className='w-4 h-4'/>
-                            </div>
-                        )
-                        :(<div className='hover:bg-gray-light cursor-pointer p-1 border-b border-gray-light  ' id={list.docId} onClick={(e)=>{handleClick(e)}}>{list.name} </div>)
-                        )}
-                        <div className='hover:bg-gray-light cursor-pointer mt-2 text-center ' id='crear' onClick={(e)=>{handleClick(e)}}>Crear lista</div>
-                </div>
-            }
-            
-        </div>
-    )
+	return (
+		<div id={product.productId} className="w-[200px] m-0-auto relative">
+			<div
+				id={product.productId}
+				className=" p-[10px] shadow bg-white flex justify-between w-full rounded-full mt-2 h-10 items-center cursor-pointer "
+				onClick={(e) => {
+					setDropdown(!dropdown);
+				}}
+			>
+				{' '}
+				Agregar a la lista
+				<img src={downarrow} className="color-black w-3" />
+			</div>
+			{dropdown && (
+				<div className="absolute p-[5px] shadow mt-0.5 text-sm w-full z-10 bg-white">
+					{lists.map((list) =>
+						list.productsId.includes(product.productId) ? (
+							<div
+								className="hover:bg-gray-light cursor-pointer flex justify-between p-1 border-b border-gray-light "
+								id="disabled"
+								onClick={(e) => {
+									handleClick(e);
+								}}
+							>
+								{list.name}
+								<img src={check} className="w-4 h-4" />
+							</div>
+						) : (
+							<div
+								className="hover:bg-gray-light cursor-pointer p-1 border-b border-gray-light  "
+								id={list.docId}
+								onClick={(e) => {
+									handleClick(e);
+								}}
+							>
+								{list.name}{' '}
+							</div>
+						),
+					)}
+					<div
+						className="hover:bg-gray-light cursor-pointer mt-2 text-center "
+						id="crear"
+						onClick={(e) => {
+							handleClick(e);
+						}}
+					>
+						Crear lista +
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 /* window.addEventListener("click", function(event) {
@@ -57,4 +87,3 @@ export function Dropdown({lists, product, handleSelect}) {
       }
     }
   }); */
-
--- wishlist/src/components/MenuAside.jsx
+++ wishlist/src/components/MenuAside.jsx
@@ -1,91 +1,91 @@
 //key
-import React from "react";
-import { Link } from "react-router-dom";
-import user from "../imgFalabella/personal-data.svg";
-import compras from "../imgFalabella/Vector.png";
-import lista from "../imgFalabella/my-lists.svg";
-import direccion from "../imgFalabella/my-addresses.svg";
-import pago from "../imgFalabella/payments-methods.svg";
-import CMR from "../imgFalabella/cmr-card.svg";
-import giftcard from "../imgFalabella/gift-cards.svg";
-import cambiarcontraseña from "../imgFalabella/passwords.svg";
-import cerrarsesion from "../imgFalabella/close-session.svg";
-import RightArrow from "../imgFalabella/RightArrow";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import user from '../imgFalabella/personal-data.svg';
+import compras from '../imgFalabella/Vector.png';
+import lista from '../imgFalabella/my-lists.svg';
+import direccion from '../imgFalabella/my-addresses.svg';
+import pago from '../imgFalabella/payments-methods.svg';
+import CMR from '../imgFalabella/cmr-card.svg';
+import giftcard from '../imgFalabella/gift-cards.svg';
+import cambiarcontraseña from '../imgFalabella/passwords.svg';
+import cerrarsesion from '../imgFalabella/close-session.svg';
+import RightArrow from '../imgFalabella/RightArrow';
 
 const MenuAside = () => {
-  let sidebarMenu = [
-    {
-      display: "Mis datos personales",
-      icon: <img src={user} />,
-      to: "/mi_datos_personales",
-      section: "/mi_datos_personales",
-    },
-    {
-      display: "Mis compras",
-      icon: <img src={compras} />,
-      to: "/Mis compras",
-      section: "Mis compras",
-    },
-    {
-      display: "Mis lista",
-      icon: <img src={lista} />,
-      to: "/lista",
-      section: "lista",
-    },
-    {
-      display: "Mis direcciones",
-      icon: <img src={direccion} />,
-      to: "/Mis direcciones",
-      section: "cMis direcciones",
-    },
-    {
-      display: "Mis medios de pago",
-      icon: <img src={pago} />,
-      to: "/Mis medios de pago",
-      section: "Mis medios de pago",
-    },
-    {
-      display: "Mi CMR",
-      icon: <img src={CMR} />,
-      to: "/Mi CMR",
-      section: "Mi CMR",
-    },
+	let sidebarMenu = [
+		{
+			display: 'Mis datos personales',
+			icon: <img src={user} />,
+			to: '/mi_datos_personales',
+			section: '/mi_datos_personales',
+		},
+		{
+			display: 'Mis compras',
+			icon: <img src={compras} />,
+			to: '/Mis compras',
+			section: 'Mis compras',
+		},
+		{
+			display: 'Mis lista',
+			icon: <img src={lista} />,
+			to: '/lista',
+			section: 'lista',
+		},
+		{
+			display: 'Mis direcciones',
+			icon: <img src={direccion} />,
+			to: '/Mis direcciones',
+			section: 'cMis direcciones',
+		},
+		{
+			display: 'Mis medios de pago',
+			icon: <img src={pago} />,
+			to: '/Mis medios de pago',
+			section: 'Mis medios de pago',
+		},
+		{
+			display: 'Mi CMR',
+			icon: <img src={CMR} />,
+			to: '/Mi CMR',
+			section: 'Mi CMR',
+		},
 
-    {
-      display: "Mis Gift cards",
-      icon: <img src={giftcard} />,
-      to: "/Mis Gift cards",
-      section: "Mis Gift cards",
-    },
-    {
-      display: "Cambiar mi contraseña",
-      icon: <img src={cambiarcontraseña} />,
-      to: "/Cambiar mi contraseña",
-      section: "Cambiar mi contraseña",
-    },
-    {
-      display: "Cerrar sesion",
-      icon: <img src={cerrarsesion} />,
-      to: "/Cerrar sesion",
-      section: "Cerrar sesion",
-    },
-  ];
-  return (
-    <>
-      <div className=" bg-white w-[321px] h-[559px] shadow-xl rounded ">
-        {sidebarMenu.map((item, index) => (
-          <Link to={item.to} key={index}>
-            <div className="flex w-full p-[21px] text-sm border-b- bg-white hover:border-l-4 shadow-outer hover:shadow-inner hover:bg-[#F0F0F0] hover:bg-opacity-100  transition-all duration-100 ease-in-out cursor-pointer ">
-              <i className="mr-[10px]">{item.icon}</i>
-              <h2 className="text-[16px] tex-[#333333]  ">{item.display}</h2>
-              <div className=" w-6 ml-auto p-1  ">
-                <RightArrow />
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </>
-  );
+		{
+			display: 'Mis Gift cards',
+			icon: <img src={giftcard} />,
+			to: '/Mis Gift cards',
+			section: 'Mis Gift cards',
+		},
+		{
+			display: 'Cambiar mi contraseña',
+			icon: <img src={cambiarcontraseña} />,
+			to: '/Cambiar mi contraseña',
+			section: 'Cambiar mi contraseña',
+		},
+		{
+			display: 'Cerrar sesion',
+			icon: <img src={cerrarsesion} />,
+			to: '/Cerrar sesion',
+			section: 'Cerrar sesion',
+		},
+	];
+	return (
+		<>
+			<div className=" bg-white w-[321px] h-[559px] shadow-xl rounded ">
+				{sidebarMenu.map((item, index) => (
+					// <Link to={item.to} key={index}>
+					<div className="flex w-full p-[21px] text-sm border-b- bg-white hover:border-l-4 shadow-outer hover:shadow-inner hover:bg-[#F0F0F0] hover:bg-opacity-100  transition-all duration-100 ease-in-out cursor-pointer ">
+						<i className="mr-[10px]">{item.icon}</i>
+						<h2 className="text-[16px] tex-[#333333]  ">{item.display}</h2>
+						<div className=" w-6 ml-auto p-1  ">
+							<RightArrow />
+						</div>
+					</div>
+					// </Link>
+				))}
+			</div>
+		</>
+	);
 };
 export default MenuAside;
